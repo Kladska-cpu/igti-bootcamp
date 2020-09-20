e0630c25565b7a98ea3e2d86c34c6fe5a1aa2e9e
Kladska-cpu <66175835+Kladska-cpu@users.noreply.github.com>
2020-09-20 12:20:26 -0300

    Delete dados.py

--- dados.py
+++
@@ -1,7 +0,0 @@
-a = 5       # int
-b = 5.0     # float
-c = 5 + 4j  # complex
-
-print(f"a ({a}) é do tipo: {type(a)}")
-print(f"b ({b}) é do tipo: {type(b)}")
-print(f"c ({c}) é do tipo: {type(c)}")
